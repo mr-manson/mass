e06d44a5c18ec26d9f1385d2b50f80caae4f549a
mr-manson <60350677+mr-manson@users.noreply.github.com>
2020-02-23 17:58:12 +0300

    Add files via upload

--- jd1-les03/src/by/htp/les03/mas/MasTask20.java
+++ jd1-les03/src/by/htp/les03/mas/MasTask20.java
@@ -1,8 +1,10 @@
 package by.htp.les03.mas;
 
 public class MasTask20 {
-
 	public static void main(String[] args) {
+		// 17. ƒана последовательность целых чисел a a a . ќбразовать новую
+		// последовательность, выбросив из исходной те члены, которые равны min(a a a).
+
 		int[] mas = new int[] { -10, 22, 31, 45, -10, 36 };
 
 		for (int i = 0; i < mas.length; i++) {
