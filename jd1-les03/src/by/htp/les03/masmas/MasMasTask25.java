53fedf199d11af09286725a03072259711c605eb
mr-manson <60350677+mr-manson@users.noreply.github.com>
2020-02-26 19:30:21 +0300

    Add files via upload

---
+++ jd1-les03/src/by/htp/les03/masmas/MasMasTask25.java
@@ -0,0 +1,35 @@
+package by.htp.les03.masmas;
+
+public class MasMasTask25 {
+
+	public static void main(String[] args) {
+
+		// 24. Дан линейный массив x x x x , , , , . Получить действительную квадратную
+		// матрицу порядка n:
+
+		int n = 6;
+		int[][] mas = new int[n][n];
+		int x = 1;
+
+		for (int i = 0; i < mas.length; i++) {
+			for (int j = 0; j < mas[i].length; j++) {
+				mas[i][j] = x;
+				x++;
+				System.out.printf("%5d", mas[i][j]);
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+	public static void masPrint(int[][] a) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
